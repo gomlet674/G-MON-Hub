830b836f76feb01894e34b9f679c969b09fc9faf
gomlet674 <faiqabqoriahmad1@gmail.com>
2025-05-18 07:34:41 +0800

    Create build.lua

---
+++ build.lua
@@ -0,0 +1,5 @@
+local GAME_SCRIPTS = {
+    [4442272183] = "https://raw.githubusercontent.com/gomlet674/G-MON-Hub/main/main.lua",         -- Blox Fruits
+    [3233893879] = "https://raw.githubusercontent.com/gomlet674/G-MON-Hub/main/main_arena.lua",   -- Arena
+    [537413528]  = "https://raw.githubusercontent.com/gomlet674/G-MON-Hub/main/build.lua",        -- Build A Boat
+}
