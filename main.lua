5cc8d7980667ce5bdca4ad573780b358a1c56728
gomlet674 <faiqabqoriahmad1@gmail.com>
2025-12-31 01:31:34 +0800

    Update game detection and GUI labels

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
--- Rayfield GUI Controller (FINAL FIX)
+-- Rayfield GUI Controller (ON/OFF WORKING)
+-- FOR YOUR OWN GAMES ONLY
 
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -7,30 +8,32 @@ local player = Players.LocalPlayer
 -- Load Rayfield
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
--- Remote (SERVER ONLY CREATE)
-local Remote = ReplicatedStorage:WaitForChild("HubRemote")
+-- Remote setup
+local Remote =
+ReplicatedStorage:WaitForChild("HubRemote")
 
--- GAME DETECTION
+-- GAME DETECTION (ubah PlaceId sesuai game kamu)
 local GAME_TYPE = "UNKNOWN"
 if game.PlaceId == 2753915549 then
     GAME_TYPE = "BLOX_FRUIT"
 elseif game.PlaceId == 1554960397 then
-    GAME_TYPE = "CAR_TYCOON"
+    GAME_TYPE = "CAR_DEALERSHIP_TYCOON"
 elseif game.PlaceId == 537413528 then
-    GAME_TYPE = "BUILD_A_BOAT"
+    GAME_TYPE = "BUILD_A_BOAT_FOR_TREASURE"
 end
 
 -- GUI
 local Window = Rayfield:CreateWindow({
-    Name = "GMON",
-    LoadingTitle = "GMON Script",
-    LoadingSubtitle = "Logic ON/OFF Aktif",
+    Name = "G-MON",
+    LoadingTitle = "G-MON Script",
+    LoadingSubtitle = "Welcome To G-MON",
 })
 
 local MainTab = Window:CreateTab("Main")
+
 MainTab:CreateLabel("Game Terdeteksi: "..GAME_TYPE)
 
--- ===== BLOX FRUIT =====
+-- ===== BLOX FRUIT LOGIC =====
 if GAME_TYPE == "BLOX_FRUIT" then
     MainTab:CreateToggle({
         Name = "Auto Farm Nearest Enemy",
@@ -41,8 +44,8 @@ if GAME_TYPE == "BLOX_FRUIT" then
     })
 end
 
--- ===== CAR DEALERSHIP TYCOON =====
-if GAME_TYPE == "CAR_TYCOON" then
+-- ===== CAR DEALERSHIP TYCOON LOGIC =====
+if GAME_TYPE == "CAR_DEALERSHIP_TYCOON" then
     MainTab:CreateToggle({
         Name = "Auto Farm Money (Car Run)",
         CurrentValue = false,
@@ -52,19 +55,19 @@ if GAME_TYPE == "CAR_TYCOON" then
     })
 end
 
--- ===== BUILD A BOAT =====
-if GAME_TYPE == "BUILD_A_BOAT" then
+-- ==== BUILD A BOAT FOR TREASURE LOGIC ====
+if GAME_TYPE == " BUILD_A_BOAT_FOR_TREASURE" then
     MainTab:CreateToggle({
-        Name = "Auto Farm Gold",
-        CurrentValue = false,
-        Callback = function(v)
-            Remote:FireServer("BOAT_GOLD_FARM", v)
-        end
-    })
-end
-
-Rayfield:Notify({
-    Title = "GMON",
-    Content = "GUI siap & terhubung ke server",
+        Name = "Auto Farm Gold", 
+        CurrentValue = false, 
+        Callback = function(v) 
+            Remote:FireServer("BOAT_GOLD_FARM", v) 
+        end   
+    }) 
+end  
+            
+Rayfield:CreateNotification({
+    Title = "Ready",
+    Content = "Welcome",
     Duration = 4
 })
